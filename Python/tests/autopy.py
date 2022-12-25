1d8b60bd1ecca222d6c117e51eedbe442b9fa7cd
Vaibhav Malaviya <55515799+vaimalaviya1233@users.noreply.github.com>
2022-12-25 00:14:11 -0600

    changes

--- Python/tests/autopy.py
+++ Python/tests/autopy.py
@@ -13,8 +13,11 @@ auto.moveTo(1600,830)
 for i in range(2000):
     position = pyautogui.position()
     if(position.x == 1600):
-        auto.moveTo(x=1600,y=830)
-        auto.scroll(-800)
-        # time.sleep(.300)
         auto.click()
+        auto.moveTo(x=1600,y=830)
+        auto.scroll(-900)
+        # time.sleep(.300)
+    else:
+        print(position.x, position.y)
+
 print("ended")
